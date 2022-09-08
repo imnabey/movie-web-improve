09da3c89fed94dd75dc379e2f9e1c1397098459e
imnabey <nabilahpermata6@gmail.com>
2022-09-08 08:58:11 +0700

    fix:remove lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import React, { FC, lazy, Suspense } from 'react';
+import { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 import MovieListContext from './contexts/MovieList';
+import Detail from './pages/Detail';
+import MyList from './pages/MyList';
+import Home from './pages/Homepage';
 import Layout from './components/Layout';
 import './styles/injectGlobal';
-
-const Home = lazy(async () => ({ default: (await import('./pages/Homepage')).Homepage }));
-const Detail = lazy(async () => ({ default: (await import('./pages/Detail')).Detail }));
-const MyList = lazy(async () => ({ default: (await import('./pages/MyList')).MyList }));
 
 export const App: FC = () => {
   return (
@@ -17,13 +16,11 @@ export const App: FC = () => {
       <BrowserRouter>
         <MovieListContext>
           <Layout>
-            <Suspense>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/detail/:id" element={<Detail />} />
-                <Route path="/my-list" element={<MyList />} />
-              </Routes>
-            </Suspense>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/detail/:id" element={<Detail />} />
+              <Route path="/my-list" element={<MyList />} />
+            </Routes>
           </Layout>
         </MovieListContext>
       </BrowserRouter>
